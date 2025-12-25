973afa5eeaeca2010ea7108394f6d0e7cbe59696
mh <119695574+mh28-feb@users.noreply.github.com>
2025-12-25 08:08:42 +0700

    Refactor AdminSystem to include AuthManager

--- src/admin.py
+++ src/admin.py
@@ -2,20 +2,18 @@ import os
 from src.book import BookManager
 from src.reader import ReaderManager
 from src.borrow_return import BorrowManager
+from src.auth import AuthManager
 from src.utils import ensure_data_dir
 
 class AdminSystem:
-    """Lớp điều phối kết nối các bộ quản lý dữ liệu."""
-    
     def __init__(self, data_dir="data"):
         ensure_data_dir(data_dir)
         
-        # Thiết lập đường dẫn file
-        books_path = os.path.join(data_dir, "books.json")
-        readers_path = os.path.join(data_dir, "readers.json")
-        borrows_path = os.path.join(data_dir, "borrows.json")
-
-        # Khởi tạo các Manager
-        self.book_manager = BookManager(path=books_path)
-        self.reader_manager = ReaderManager(path=readers_path)
-        self.borrow_manager = BorrowManager(borrows_path, self.book_manager, self.reader_manager)
+        self.auth_manager = AuthManager(os.path.join(data_dir, "users.json"))
+        self.book_manager = BookManager(path=os.path.join(data_dir, "books.json"))
+        self.reader_manager = ReaderManager(path=os.path.join(data_dir, "readers.json"))
+        self.borrow_manager = BorrowManager(
+            os.path.join(data_dir, "borrows.json"), 
+            self.book_manager, 
+            self.reader_manager
+        )
